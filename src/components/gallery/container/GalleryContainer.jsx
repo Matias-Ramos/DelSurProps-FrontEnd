9d22c35db239088c0cbb421f7b923eda8f79869d
MatiasR <noemail@noemail.com>
2023-07-20 18:56:49 -0300

    work in progress

--- src/components/gallery/container/GalleryContainer.jsx
+++ src/components/gallery/container/GalleryContainer.jsx
@@ -1,13 +1,37 @@
 import Card from "../cards/Card.jsx";
-import { data } from "../../../dummyData.js";
-const GalleryContainer = () => (
+
+//import { data } from "../../../dummyData.js";
+import { useState, useEffect } from "react";
+
+const GalleryContainer = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/Emprendimientos");
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("test");
+    console.log(data);
+  }, [data]);
+
   <div id="cardsContainer">
     {data.map((building) => (
       <div key={building.id} className="cardOuterBackground">
         <Card building={building} />
       </div>
     ))}
-  </div>
-);
+  </div>;
+};
 
 export default GalleryContainer;
